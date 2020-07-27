71fdff2e33283193509aa312de7c22d7cb3c117d
胡晓 <hux@huonu.com>
2020-07-27 17:04:16 +0800

    fix

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Utils = {
+  Caches: require('./lib/caches'),
+  File: require('./lib/file'),
+  Logger: require('./lib/logger'),
+  String: require('./lib/string'),
+  Time: require('./lib/time'),
+  Common: require('./lib/index'),
+  Validate: require('./lib/validate'),
+};
+
+module.exports = Utils;

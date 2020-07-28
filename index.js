10811a260d58d5199d5c4895a8ae340519af5102
胡晓 <hux@huonu.com>
2020-07-28 16:38:59 +0800

    fix

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const Utils = {
+module.exports = {
   array: require('./lib/array'),
   caches: require('./lib/caches'),
-  common: require('./lib/index'),
+  common: require('./lib/common'),
   file: require('./lib/file'),
   html: require('./lib/html'),
   logger: require('./lib/logger'),
   random: require('./lib/random'),
+  scrollTo: require('./lib/scrollTo'),
   string: require('./lib/string'),
   time: require('./lib/time'),
   validate: require('./lib/validate'),
 };
-
-module.exports = Utils;

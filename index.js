740968c2e6ad12790cd6814716cd77798caa8932
prtens <hux@huonu.com>
2020-07-29 18:06:38 +0800

    'fix

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  array: require('./lib/array'),
+  caches: require('./lib/caches'),
+  common: require('./lib/common'),
+  file: require('./lib/file'),
+  html: require('./lib/html'),
+  logger: require('./lib/logger'),
+  random: require('./lib/random'),
+  scrollTo: require('./lib/scrollTo'),
+  string: require('./lib/string'),
+  time: require('./lib/time'),
+  validate: require('./lib/validate'),
+};

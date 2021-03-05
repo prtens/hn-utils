5cdf893d2d5361f1357f660853e2f6f423f129b7
prtens <hux@huonu.com>
2021-03-05 13:54:48 +0800

    fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
   file: require('./lib/file'),
   html: require('./lib/html'),
   logger: require('./lib/logger'),
+  pickerOptions: require('./lib/pickerOptions'),
   random: require('./lib/random'),
   scrollTo: require('./lib/scrollTo'),
   string: require('./lib/string'),
